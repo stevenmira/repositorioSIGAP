dceb5c7a823260fc3477940fc2af65251b02d3ea
Oscar Turish <ortg_10@hotmail.com>
2018-06-15 16:10:01 -0600

    menus

--- SIGAP/resources/views/menu/tactico.blade.php
+++ SIGAP/resources/views/menu/tactico.blade.php
@@ -1,37 +1,35 @@
-            <li class="treeview">
+
+           <li class="treeview">
               <a href="#">
-                <i class="fa fa-file-text"></i>
-                <span>Reportes Tacticos</span>
-                 <i class="fa fa-angle-left pull-right"></i>
+                <i class="fa fa-info-circle"></i>
+                <span>Reportes Tactico</span>
+                 
               </a>
-              <ul class="treeview-menu">
-               <li><a  href="{{URL::action('CarteraClienteController@create')}}"><i class="fa fa-circle-o">
-               </i>Cartera de Clientes</a></li>
-               <li><a  
-                href="{{URL::action('CreditoCompletoController@create')}}"><i class="fa fa-circle-o">
-                </i>Creditos Completo 
-                </a></li>
-               <li><a  href=""><i class="fa fa-circle-o"></i>Clientes Morosos </a></li>
-               <li><a  href=""><i class="fa fa-circle-o"></i>Contratos Vencidos</a></li>
-               <li><a  href=""><i class="fa fa-circle-o"></i>Clasificacion de Clientes</a></li>
-               <li><a  href=""><i class="fa fa-circle-o"></i>Clasificacion de Ejecutivos</a></li>
-              </ul>
             </li>
             <li>
               <a href="">
-                <i class="fa fa-plus-square"></i> 
-                <span>Ayuda</span>
-                <small class="label pull-right bg-red">PDF</small>
+                <i class="fa fa-file-text"></i> <span>Cartera de Clientes</span>
+               
               </a>
-              <ul class="treeview-menu">
-
-                <li><a  href=""><i class="fa fa-circle-o"></i> Manual de Usuario</a></li>
-
-              </ul>
+            </li>
+            <li>
+              <a href="">
+                <i class="fa fa-file-text"></i> <span>Creditos Completo</span>
+                
+              </a>
             </li>
             <li>
               <a href="#">
-                <i class="fa fa-info-circle"></i> <span>Acerca De...</span>
-                <small class="label pull-right bg-yellow">IT</small>
+                <i class="fa fa-file-text"></i> <span>Clientes Morosos</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-file-text"></i> <span>Contratos Vencidos</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-file-text"></i> <span><small>Clasificacion de Ejecutivos</small></span>
               </a>
             </li>
