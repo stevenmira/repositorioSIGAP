f3fe8c0f9b0a86a4029c6d2d18247c5ea0d340c3
Alex <luis_grennby92@hotmail.com>
2018-06-15 03:59:50 -0600

    correccion en el orde de creado de migraciones

--- SIGAP/database/migrations/2018_06_14_133705_ETL.php
+++ SIGAP/database/migrations/2018_06_14_133705_ETL.php
@@ -14,27 +14,81 @@ class ETL extends Migration
     {
 
 
-        Schema::connection('pgsql')->create('detalle_liquidacion', function($table)
+        Schema::connection('pgsql')->create('prestamo', function($table)
         {
 
-            $table->integer('idcuenta')->unsigned();
-            $table->index('idcuenta');
-            $table->foreign('idcuenta')->references('idcuenta')->on('cuenta')->onDelete('cascade');
+            $table->increments('idprestamo');
+            $table->float('monto')->required();
+            $table->float('cuotadiaria')->required();
+            $table->date('fechaultimapago')->nullable();
+            $table->timestamps();
+        });
 
-            $table->increments('iddetalleliquidacion');
-            $table->date('fechaefectiva')->nullable();
-            $table->float('monto')->nullable();
-            $table->decimal('cuotacapital',6, 2)->nullable();
-            $table->decimal('totaldiario',6, 2)->nullable();
-            $table->float('interes')->nullable();
-            $table->string('estado',10)->nullable();
-            $table->date('fechadiaria')->nullable();
-            $table->string('abonocapital',10)->nullable();
+        
+        Schema::connection('pgsql')->create('ejecutivo', function($table)
+        {
+            $table->increments('idejecutivo');
+            $table->string('nombreejecutivo',50)->required();
+            $table->string('supervisor',50)->required();
+            $table->string('clasificacion',1)->required();
+            $table->timestamps();
+        });
+
+
+        Schema::connection('pgsql')->create('carteraExtendida', function($table)
+        {
+            $table->increments('idcarexte');
+            $table->float('saldocapital');
+            $table->float('moraexte');
+            $table->date('fecha')->nullable();
+            $table->string('intetotal',10)->required();
+            $table->timestamps();
+        });
+
+
+        Schema::connection('pgsql')->create('cartera', function($table)
+        {
+
+            $table->integer('idejecutivo')->unsigned();
+            $table->index('idejecutivo');
+            $table->foreign('idejecutivo')->references('idejecutivo')->on('ejecutivo')->onDelete('cascade');
+
+            $table->increments('idcartera');
+            $table->string('nombre',50)->required();
+            $table->timestamps();
+        });
+
+        Schema::connection('pgsql')->create('cliente', function($table)
+        {
+
+            $table->integer('idcartera')->unsigned();
+            $table->index('idcartera');
+            $table->foreign('idcartera')->references('idcartera')->on('cartera')->onDelete('cascade');
+
+            $table->increments('idcliente');
+            
+            $table->string('nombre',50)->required();
+            $table->string('apellido',50)->required();
+            $table->string('dui',10)->required();
+            $table->string('nit',17)->required();
+            $table->string('estado',10);
             $table->timestamps();
         });
 
 
 
+        Schema::connection('pgsql')->create('negocio', function($table)
+        {
+
+            $table->integer('idcliente')->unsigned();
+            $table->index('idcliente');
+            $table->foreign('idcliente')->references('idcliente')->on('cliente')->onDelete('cascade');
+
+            $table->increments('idnegocio');
+            $table->string('nombre',50)->required();
+            $table->boolean('estadomora');
+            $table->timestamps();
+        });
 
         Schema::connection('pgsql')->create('cuenta', function($table)
         {
@@ -55,76 +109,33 @@ class ETL extends Migration
         });
 
 
-        Schema::connection('pgsql')->create('negocio', function($table)
+        
+
+
+        
+
+       
+
+      
+       
+
+
+        Schema::connection('pgsql')->create('detalle_liquidacion', function($table)
         {
 
-            $table->integer('idcliente')->unsigned();
-            $table->index('idcliente');
-            $table->foreign('idcliente')->references('idcliente')->on('cliente')->onDelete('cascade');
+            $table->integer('idcuenta')->unsigned();
+            $table->index('idcuenta');
+            $table->foreign('idcuenta')->references('idcuenta')->on('cuenta')->onDelete('cascade');
 
-            $table->increments('idnegocio');
-            $table->string('nombre',50)->required();
-            $table->boolean('estadomora');
-            $table->timestamps();
-        });
-
-
-        Schema::connection('pgsql')->create('cliente', function($table)
-        {
-
-            $table->integer('idcliente')->unsigned();
-            $table->index('idcliente');
-            $table->foreign('idcliente')->references('idcliente')->on('cliente')->onDelete('cascade');
-
-            $table->increments('idcliente');
-            
-            $table->string('nombre',50)->required();
-            $table->string('apellido',50)->required();
-            $table->string('dui',10)->required();
-            $table->string('nit',17)->required();
-            $table->string('estado',10);
-            $table->timestamps();
-        });
-
-        Schema::connection('pgsql')->create('prestamo', function($table)
-        {
-
-            $table->increments('idprestamo');
-            $table->float('monto')->required();
-            $table->float('cuotadiaria')->required();
-            $table->date('fechaultimapago')->nullable();
-            $table->timestamps();
-        });
-
-        Schema::connection('pgsql')->create('cartera', function($table)
-        {
-
-            $table->integer('idejecutivo')->unsigned();
-            $table->index('idejecutivo');
-            $table->foreign('idejecutivo')->references('idejecutivo')->on('ejecutivo')->onDelete('cascade');
-
-            $table->increments('idcartera');
-            $table->string('nombre',50)->required();
-            $table->timestamps();
-        });
-
-        Schema::connection('pgsql')->create('ejecutivo', function($table)
-        {
-            $table->increments('idejecutivo');
-            $table->string('nombreejecutivo',50)->required();
-            $table->string('supervisor',50)->required();
-            $table->string('clasificacion',1)->required();
-            $table->timestamps();
-        });
-
-
-        Schema::connection('pgsql')->create('carteraExtendida', function($table)
-        {
-            $table->increments('idcarexte');
-            $table->float('saldocapital');
-            $table->float('moraexte');
-            $table->date('fecha')->nullable();
-            $table->string('intetotal',10)->required();
+            $table->increments('iddetalleliquidacion');
+            $table->date('fechaefectiva')->nullable();
+            $table->float('monto')->nullable();
+            $table->decimal('cuotacapital',6, 2)->nullable();
+            $table->decimal('totaldiario',6, 2)->nullable();
+            $table->float('interes')->nullable();
+            $table->string('estado',10)->nullable();
+            $table->date('fechadiaria')->nullable();
+            $table->string('abonocapital',10)->nullable();
             $table->timestamps();
         });
 
