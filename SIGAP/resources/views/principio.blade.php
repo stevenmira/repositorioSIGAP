3db8988635b44f088d2b41d798dcc1c05645a154
Oscar Turish <ortg_10@hotmail.com>
2018-06-27 16:46:21 -0600

    pantalla de inicio

---
+++ SIGAP/resources/views/principio.blade.php
@@ -0,0 +1,38 @@
+@extends ('layouts.inicio')
+@section('contenido')
+<div class="clearfix">
+                            <h2 style="text-align:center" ><b>BIENVENIDO USUARIO</b></h2></div>
+                           <div><h3 style="text-align:center"><b>{{$usuarioactual->nombre}}</b></h3></div> 
+                          <div style="text-align:center">
+                          <img src="../img/logo.png"  width="300" height="300">
+                          </div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            
+                <div class="panel-heading">
+                   </div>
+                <div class="panel-body">
+                    <div class="row">
+                    
+                        <div style="text-align:center" class="col-xs-11 col-md-11">
+                       
+      
+      
+
+          
+        </div>
+
+
+                        
+                                                
+                    </div>
+                    
+                </div>
+            
+        </div>
+    </div>
+</div>
+
+@endsection
